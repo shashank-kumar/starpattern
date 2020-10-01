80a902931eed922775ca5dede4688831b2c7e8ee
Shashank Kumar <shashank-kumar@users.noreply.github.com>
2020-10-02 03:53:18 +0530

    Update codeofstar.py
    There are two changes - 
    1. According to python variable naming convention, all variables should be written in lowercase letters.
    2. Minor code formatting fixes.

--- codeofstar.py
+++ codeofstar.py
@@ -2,17 +2,17 @@ print("\t \t \t \t \t \t \t \t \t \t ** CodeOfStar **")
 
 print("Input q for quit.\n")
 
-Programe_over = False
+program_over = False
 
-while Programe_over is False:
-    n = input("How many times do you want to print star pattern:")
+while program_over is False:
+    n = input("\nHow many times do you want to print star pattern: ")
 
     if n == "q":
-        Programe_over = True
+        program_over = True
     else:
         print("\n Code Of Star Pattern \n ")
 
-        boolean = int(input("Press 1 for decreasing order and 0 for increasing order:"))
+        boolean = int(input("Press 1 for decreasing order and 0 for increasing order: "))
         number = int(n)
         new_n = number + 1
 
@@ -23,23 +23,23 @@ while Programe_over is False:
                 return False
 
         if number< 0:
-            print("Your input is a negative number")
+            print("Your input is a negative number\n")
 
         else:
             if boolean == 0 or boolean == 1:
 
                 if convert_bool(boolean):
-                    print("\n * pattern in ascending order")
+                    print("\n* pattern in ascending order\n")
 
                     for times in range(new_n):
                         print("*" * times)
 
                 elif not convert_bool(boolean):
-                    print("* pattern in decending order")
+                    print("\n* pattern in decending order\n")
 
                     # This thing can be done by for loop but it become complex
                     while number >= 1:
                         print("*" * number)
                         number = number - 1
             else:
-                print("Sorry you have to only input 0 or 1")
+                print("Sorry, you have to input 0 or 1 only")
